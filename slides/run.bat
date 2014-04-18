228e43fb6b9e2c05f25fff8389373f1a294c3033
Manuel Boillod <manuel.boillod@zenika.com>
2014-04-18 20:47:04 +0200

    Ajout des lanceurs pour les serveurs web

---
+++ slides/run.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Launched server - Connect to 'http://localhost:8000'
+../server/server.exe
---
+++ slides/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Launched server"
+# test server (if not installed, echo command line)
+
+# run server
+../server/server
